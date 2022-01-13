b5317d2ad4d8efacb3f3d76dc39b491b3f09bfad
Naima Hassan <naima.hassan434@gmail.com>
2022-01-13 00:52:30 +0000

    useRef Ex3-Memory Leak Error Fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import UseRefEx2 from "./components/UseRefEx2";
+import UseRefEx3 from "./components/UseRefEx3";
 
 function App() {
   return (
     <div className="container mt-5">
-      <h1>Hooks</h1>
-      <UseRefEx2/>
+  
+      <UseRefEx3/>
     </div>
   );
 }
---
+++ src/components/UseRefEx3.jsx
@@ -0,0 +1,15 @@
+import React,{useState} from 'react'
+import Todo from './Todo'
+
+function UseRefEx3() {
+   const [showTodo, setShowTodo] = useState(true)
+   return (
+      <div>
+         
+         {showTodo && <Todo />}
+         <button className="btn btn-primary" onClick={()=>setShowTodo(!showTodo)}>Toggle Todo</button>
+      </div>
+   )
+}
+
+export default UseRefEx3
